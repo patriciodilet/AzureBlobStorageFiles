1e9d45b8cea6e3c88d49f7c1e69b5986653129ab
AIEP\Patricio Diaz <Patricio Daz>
2022-08-23 09:41:57 -0400

    create, read, download and delete files to azure blob storage

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+Console.WriteLine("Azure Blob Storage exercise\n");
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync().GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+static async Task ProcessAsync()
+{
+    // Copy the connection string from the portal in the variable below.
+    string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=az204storageaccount101;AccountKey=KkTeDKnAepvT1bepb3d1cUS8MMVCj/+C+YMu15j27+wHOxT6LKadVnaf9vCEnbYb08VCSum+3WtL+ASt5bOUsA==;EndpointSuffix=core.windows.net";
+
+    // Create a client that can authenticate with a connection string
+    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+
+    //Create a unique name for the container
+    string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+    // Create the container and return a container client object
+    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+    Console.WriteLine("A container named '" + containerName + "' has been created. " +
+        "\nTake a minute and verify in the portal." + 
+        "\nNext a file will be created and uploaded to the container.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    // Create a local file in the ./data/ directory for uploading and downloading
+    string localPath = "./data/";
+    string fileName = "wtfile" + Guid.NewGuid().ToString() + ".txt";
+    string localFilePath = Path.Combine(localPath, fileName);
+
+    // Write text to the file
+    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+    // Get a reference to the blob
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+    // Open the file and upload its data
+    using FileStream uploadFileStream = File.OpenRead(localFilePath);
+    await blobClient.UploadAsync(uploadFileStream, true);
+
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" + 
+            " the blobs next.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    // List blobs in the container
+    Console.WriteLine("Listing blobs...");
+    await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+    {
+        Console.WriteLine("\t" + blobItem.Name);
+    }
+
+    Console.WriteLine("\nYou can also verify by looking inside the " + 
+            "container in the portal." +
+            "\nNext the blob will be downloaded with an altered file name.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+
+    // Download the blob to a local file
+    // Append the string "DOWNLOADED" before the .txt extension 
+    string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOADED.txt");
+
+    Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+    // Download the blob's contents and save it to a file
+    BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+    using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+    {
+        await download.Content.CopyToAsync(downloadFileStream);
+    }
+    Console.WriteLine("\nLocate the local file in the data directory created earlier to verify it was downloaded.");
+    Console.WriteLine("The next step is to delete the container and local files.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+
+    // Delete the container and clean up local files created
+    Console.WriteLine("\n\nDeleting blob container...");
+    await containerClient.DeleteAsync();
+
+    Console.WriteLine("Deleting the local source and downloaded files...");
+    File.Delete(localFilePath);
+    File.Delete(downloadFilePath);
+
+    Console.WriteLine("Finished cleaning up.");
+
+}
+
+private static async Task ReadContainerPropertiesAsync(BlobContainerClient container)
+{
+    try
+    {
+        // Fetch some container properties and write out their values.
+        var properties = await container.GetPropertiesAsync();
+        Console.WriteLine($"Properties for container {container.Uri}");
+        Console.WriteLine($"Public access level: {properties.Value.PublicAccess}");
+        Console.WriteLine($"Last modified time in UTC: {properties.Value.LastModified}");
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+public static async Task AddContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        IDictionary<string, string> metadata =
+           new Dictionary<string, string>();
+
+        // Add some metadata to the container.
+        metadata.Add("docType", "textDocuments");
+        metadata.Add("category", "guidance");
+
+        // Set the container's metadata.
+        await container.SetMetadataAsync(metadata);
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+public static async Task ReadContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        var properties = await container.GetPropertiesAsync();
+
+        // Enumerate the container's metadata.
+        Console.WriteLine("Container metadata:");
+        foreach (var metadataItem in properties.Value.Metadata)
+        {
+            Console.WriteLine($"\tKey: {metadataItem.Key}");
+            Console.WriteLine($"\tValue: {metadataItem.Value}");
+        }
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
